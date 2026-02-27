f625ae55e48cf27e639d9f5b4b06c64ac0d54609
Aram22-Py <78864598+Aram22-Py@users.noreply.github.com>
2026-02-26 20:37:34 -0600

    Delete 02-Data_types_comissions_calculator.py

--- 02-Data_types_comissions_calculator.py
+++
@@ -1,8 +0,0 @@
-#We have different types of data
-#string (str) - "hola", "#$%#$", " ", "123"
-#integer(int) - 5,150, 963 , -3
-#floating(float) -  1.25,  205.3,  -4.5
-#lists(list) - ["salt", 1 , -3, "beach", 0]
-#dictionaries (dic) - {'color':'rojo', 'arte':'cine'}
-#Tuples(tuple) - ('mon', 'tue', 'wed')
-#sets(set) {'a','b','c','d','e'}
