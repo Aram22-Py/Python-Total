1d1c1d169c224c98eb3cd8ebe44264463f8ac8c7
Aram22-Py <78864598+Aram22-Py@users.noreply.github.com>
2026-02-27 14:40:00 -0600

    Update project_day2_comissions_calculator.py

--- 02-Data types/project_day2_comissions_calculator.py
+++ 02-Data types/project_day2_comissions_calculator.py
@@ -1,1 +1,8 @@
 #Day 2 project: Comissions calculator
+print("Hello! If you would like to calculate the amount of your commissions, please enter your full name and the number of sales for the month.")
+full_name = input("Enter your full name: ")
+number_of_sales = input("Enter the number of sales for the month: ")
+calculated_commission = int(number_of_sales) * (13/100)
+
+print(f"Name: {full_name}\nNumber of sales: {number_of_sales}")
+print(f"The commission you are entitled to is: {round(calculated_commission, 2)}\nThank you for using our services.")
