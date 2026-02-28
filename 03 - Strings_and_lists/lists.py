21bd113f869bf4d4ba5ef9ed067f417e903d286e
Aram22-Py <78864598+Aram22-Py@users.noreply.github.com>
2026-02-27 20:11:30 -0600

    Update and rename lists.py to lists_dictionaries.py

--- 03 - Strings_and_lists/lists.py
+++
@@ -1,1 +0,0 @@
-#some LISTS properties
---
+++ 03 - Strings_and_lists/lists_dictionaries.py
@@ -0,0 +1,11 @@
+#some LISTS properties
+mi_lista= [1,"dos", True, "nope", -5]
+
+#Agrega el elemento "motocicleta" a la siguiente lista de medios de transporte:
+medios_transporte = ["avión", "auto", "barco", "bicicleta"]
+medios_transporte.append("motocicleta")
+
+#Utiliza el método pop() para quitar el tercer elemento de la siguiente lista llamada frutas, y almacénalo en una variable llamada eliminado.
+frutas = ["manzana", "banana", "mango", "cereza", "sandía"]
+eliminado = frutas.pop(2)
+print(eliminado)
