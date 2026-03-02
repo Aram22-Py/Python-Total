2de58d189143d0380f981962e8aba0ec385ded50
Aram22-Py <78864598+Aram22-Py@users.noreply.github.com>
2026-03-02 13:08:21 -0600

    Create boolleans.py

---
+++ 03 - Strings_and_lists/boolleans.py
@@ -0,0 +1,7 @@
+#Boolleans
+prueba = 5 == 6 #basically this is a True/False question
+print(prueba) #outcome is 'False'
+#example
+print((17834/34)>(87*56)) #outcome is 'False'
+#example
+print((25**0.5)==5) #outcome is 'True'
