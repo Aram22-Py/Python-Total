9137f5bc3a5487c48b16f2773ad199308c776968
Aram22-Py <78864598+Aram22-Py@users.noreply.github.com>
2026-03-13 15:15:12 -0600

    Update pathlib_path.py

--- 06 - Files and Pathlib/pathlib_path.py
+++ 06 - Files and Pathlib/pathlib_path.py
@@ -1,3 +1,9 @@
 #Now we will see the advantages for PATH
 from pathlib import Path
 ruta_base=Path.home()
+
+# ABSOLUTE route to get to the final file
+ruta=Path(ruta_base,"Curso Python","Día 6","practicas_path.py")
+
+# RELATIVE route to get to the final file, does NOT contain the home
+ruta=Path("Curso Python","Día 6","practicas_path.py")
